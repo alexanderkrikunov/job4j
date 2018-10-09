4d2f44aacf8de488ce05e4c083d8423a86377e7d
Alexander Krikunov <krikunov1990@mail.ru>
2018-10-09 21:44:24 +0700

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+*Calculate.
+*
+*@autor Alexander Krikunov (krikunov1990@mail.ru)
+*/
+
 public class Calculate{
+
+	/**
+	*Main.
+	*param args - args.
+	*/
+
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
