ee070f6cf7db31c458a094a972a94b6279ab3589
Alexander Krikunov <krikunov1990@mail.ru>
2018-10-09 22:52:48 +0700

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*Test.
+*
+*@autor Alexander Krikunov (krikunov1990@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+
+public class CalculateTest{
+
+/**
+*Test echo.
+*/
+@Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Alexander Krikunov";
+    String expect = "Echo, echo, echo : Alexander Krikunov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+    }
+}
