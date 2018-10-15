baec78296be77c96adeb225b726118ee10c85de3
Alexander Krikunov <krikunov1990@mail.ru>
2018-10-15 23:31:08 +0700

    5.4. Построить пирамиду в псевдографике. [#4412]

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -42,4 +42,20 @@ public class PaintTest {
             )
         );
     }
+
+    @Test
+    public void pyramid() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst, is(
+                new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("   ^   ")
+                .add("  ^^^  ")
+                .add(" ^^^^^ ")
+                .add("^^^^^^^")
+                .toString()
+            )
+        );
+    }
 }
