0f7e3d81119677f5bdeca9a16495e82b9f6dc68c
Alexander Krikunov <krikunov1990@mail.ru>
2018-10-16 23:25:27 +0700

    6.3. Массив заполнен true или false [#53857]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * Массив заполнен true или false.
+ *
+ * @author Alexander Krikunov (krikunov1990@mail.ru)
+ */
+public class Check {
+
+    /**
+     * перебор массива на соответствие.
+     * @param data массив.
+     * @return результат.
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+        for (int i = 0; i < data.length; i++) {
+            if (){
+
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Alexander Krikunov (krikunov1990@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
